76833a0bae0c0404cce0fa5f3b9ebeb0c5c8622f
Renato Lessa <renatolessa.2011@hotmail.com>
2017-08-05 00:58:51 -0300

    Verificação e testes de registros

--- src/br/com/farmacia/DAO/FornecedoresDAO.java
+++ src/br/com/farmacia/DAO/FornecedoresDAO.java
@@ -20,9 +20,39 @@ public class FornecedoresDAO {
 		comando.setString(1, f.getDescricao());
 		comando.executeUpdate();
 	}
+	
+	public void excluir(Fornecedores f) throws SQLException{
+		StringBuilder sql = new StringBuilder();
+		sql.append("DELETE FROM fornecedores ");
+		sql.append("WHERE codigo = ? ");
+		
+		Connection conexao = ConexaoFactory.conectar();
+		
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		comando.setLong(1, f.getCodigo());
+		comando.executeUpdate();
+	}
+	
+	public void editar(Fornecedores f) throws SQLException{
+		StringBuilder sql = new StringBuilder();
+		sql.append("UPDATE fornecedores ");
+		sql.append("SET descricao = ? ");
+		sql.append("WHERE codigo = ? ");
+		
+		Connection conexao = ConexaoFactory.conectar();
+		
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		
+		comando.setString(1, f.getDescricao());
+		comando.setLong(2, f.getCodigo());
+		comando.executeUpdate();
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		
-		Fornecedores f1 = new Fornecedores();
+	/*	Fornecedores f1 = new Fornecedores();
 		f1.setDescricao("Renato Relesi");
 		
 		Fornecedores f2 = new Fornecedores();
@@ -37,6 +67,33 @@ public class FornecedoresDAO {
 			System.out.println("Salvo com sucesso!");
 		} catch (SQLException e) {
 			System.out.println("Erro ao salvar!");
+			e.printStackTrace();
+		}*/
+		
+		/*Fornecedores f1 = new Fornecedores();
+		f1.setCodigo(3);
+		
+		FornecedoresDAO fdao = new FornecedoresDAO();
+		
+		try {
+			fdao.excluir(f1);		
+			System.out.println("Deletado com sucesso!");
+		} catch (SQLException e) {
+			System.out.println("Erro ao Deletar!");
+			e.printStackTrace();
+		}*/
+		
+		Fornecedores f1 = new Fornecedores();
+		f1.setCodigo(1);
+		f1.setDescricao("Renato Triad System");
+		
+		FornecedoresDAO fdao = new FornecedoresDAO();
+		
+		try {
+			fdao.editar(f1);		
+			System.out.println("Editado com sucesso !");
+		} catch (SQLException e) {
+			System.out.println("Erro ao Editar!");
 			e.printStackTrace();
 		}
 	
