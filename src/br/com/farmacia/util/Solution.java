f1b3d110f6537ca9efacaf985bde66df63f92a8e
Renato Lessa <renatolessa.2011@hotmail.com>
2017-08-05 23:28:45 -0300

    Listando os dados da tabela

--- src/br/com/farmacia/util/Solution.java
+++ src/br/com/farmacia/util/Solution.java
@@ -4,23 +4,22 @@ import java.util.Scanner;
 
 public class Solution {
 
-	 public static void main(String[] args){
-		    
-	        Scanner ent = new Scanner(System.in);
-	        int num, fat = 1;
-	        int cont = 1;
+	public static void main(String[] args) {
+		
+	
+		int pedra = 1,  contPedra= 1;
 
-	        do{
-	            System.out.println("Digite um nº");
-	            num = ent.nextInt();
-	            
-	            for(int i = 1;i <= num; i++){
-	                fat = fat * i;
-	            }
-	            
-	            System.out.println("!" + num + " = " + fat);
-	            cont++;
-	            
-	       }while(cont < 2);
-	    }
-}
+	
+		System.out.println("Didite  ");
+			
+	
+
+			if (pedra < 0) {
+				System.out.println("0");
+			} else if (pedra < 0) {
+				System.out.println("abcdefghijklmnopqrstuvwxyz");
+			}
+		}
+
+	}
+
