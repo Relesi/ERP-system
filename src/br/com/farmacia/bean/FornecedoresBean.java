052d8ae24abbd9c3c1fa9efa9a916dcc646c7b97
Renato Lessa <renatolessa.2011@hotmail.com>
2017-08-10 11:03:56 -0300

    Configurando alert growl

--- src/br/com/farmacia/bean/FornecedoresBean.java
+++ src/br/com/farmacia/bean/FornecedoresBean.java
@@ -10,6 +10,7 @@ import javax.faces.model.ListDataModel;
 
 import br.com.farmacia.DAO.FornecedoresDAO;
 import br.com.farmacia.domain.Fornecedores;
+import br.com.farmacia.util.JSFUtil;
 
 @ManagedBean(name = "MBFornecedores")
 @ViewScoped
@@ -43,6 +44,7 @@ public class FornecedoresBean {
 			ArrayList<Fornecedores> lista = fdao.listar();
 			itens = new ListDataModel<Fornecedores>(lista);
 		} catch (Exception e) {
+			JSFUtil.adcicionarMensagemErro("ex.getMessage()");
 			e.printStackTrace();
 		}
 	}
@@ -60,8 +62,9 @@ public class FornecedoresBean {
 			//FornecedoresDAO dao = new FornecedoresDAO();
 			ArrayList<Fornecedores> lista = fdao.listar();
 			itens = new ListDataModel<Fornecedores>(lista);
+			JSFUtil.adcicionarMensagemSucesso("Forncedor salvo com sucesso");
 		} catch (SQLException e) {
-
+			JSFUtil.adcicionarMensagemErro("ex.getMessage()");
 			e.printStackTrace();
 		}
 	}
