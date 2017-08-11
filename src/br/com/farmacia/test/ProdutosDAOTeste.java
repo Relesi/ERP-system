d85c27089f4983ac11127c138d42064970525dfd
Renato Lessa <renatolessa.2011@hotmail.com>
2017-08-11 10:20:56 -0300

    Listando e debugando com JUnit

--- src/br/com/farmacia/test/ProdutosDAOTeste.java
+++ src/br/com/farmacia/test/ProdutosDAOTeste.java
@@ -1,17 +1,24 @@
 package br.com.farmacia.test;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import br.com.farmacia.DAO.FornecedoresDAO;
 import br.com.farmacia.DAO.ProdutoDAO;
 import br.com.farmacia.domain.Fornecedores;
 import br.com.farmacia.domain.Produtos;
+import br.com.farmacia.factory.ConexaoFactory;
 
 public class ProdutosDAOTeste {
 
 	@Test
+	@Ignore
 	public void salvar()throws SQLException{
 
 		Produtos p1 = new Produtos();
@@ -30,4 +37,14 @@ public class ProdutosDAOTeste {
 
 		
 	}
+	@Test
+	public void listar()throws SQLException{
+		
+		ProdutoDAO fdao = new ProdutoDAO();
+		ArrayList<Produtos> lista = fdao.listar();
+		
+	}
+	
+	
+
 }
