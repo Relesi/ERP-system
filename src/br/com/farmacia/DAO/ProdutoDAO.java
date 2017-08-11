0a662d284fd05c17a921f2d6a4ae182acda4c9c5
Renato Lessa <renatolessa.2011@hotmail.com>
2017-08-11 08:09:45 -0300

    Criando métodos para produtos

---
+++ src/br/com/farmacia/DAO/ProdutoDAO.java
@@ -0,0 +1,27 @@
+package br.com.farmacia.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import br.com.farmacia.domain.Produtos;
+import br.com.farmacia.factory.ConexaoFactory;
+
+public class ProdutoDAO {
+
+	public void salvar(Produtos p) throws SQLException {
+		StringBuilder sql = new StringBuilder();
+		sql.append("INSERT INTO produtos ");
+		sql.append("(descricao, quantidade, preco, fornecedores_codigo)");
+		sql.append("VALUES (?,?,?,?)");
+
+		Connection conexao = ConexaoFactory.conectar();
+		
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		
+		comando.setString(1, p.getDescricao());
+		comando.setDouble(2, p.getPreco());
+		comando.setLong(3, p.getQuantidade());
+		comando.setLong(4, p.getFornecedores().getCodigo());
+		comando.executeUpdate();
+	}
+}
